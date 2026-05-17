20e5d082d7edf39a8adc9e2ee39aef55525958b2
estebansar <sar17008@byui.edu>
2026-05-16 22:26:46 -0600

    Complete Swagger API documentation setup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 
 const express = require("express");
+const swaggerUi = require("swagger-ui-express"); // lesson 4: I lets us show Swagger docs in the browser
+const swaggerDocument = require("./swagger-output.json"); // lesson4: I generated Swagger documentation file
 
 // import DB connection
 const { connectDB } = require("./db/connect");// lesson 2_connecing this to mongoDB
@@ -10,6 +12,8 @@ app.use(express.json()); // lesson 3: i will use this to allow Express to read J
 const PORT = process.env.PORT || 3000; //we set a port
 
 const routes = require("./routes"); //import rotures
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // lesson 4: Swagger page route
 
 app.use("/", routes); //use routes
 
